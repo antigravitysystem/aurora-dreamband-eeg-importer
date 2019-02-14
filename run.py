46d927a6e682c37b058d77b97e5b4307cae94bea
Jheng-lin (Johnny) Jiang <nick2nkite@hotmail.com>
2019-02-14 14:14:15 +0800

    move variables

--- run.py
+++ run.py
@@ -12,10 +12,9 @@ def allowed_file(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# @app.route('/')
-# def index():
-#      return render_template('index.html', title='Welcome')
+importer = aurora.SESSION_IMPORTER()
 
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
@@ -32,9 +31,10 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            importer = aurora.SESSION_IMPORTER()
+
             importer.run_import()
-            return redirect(url_for('upload_file', filename=filename))
+
+            return redirect(url_for('index', filename=filename))
     return render_template('index.html', title='Welcome')
 
 try:
