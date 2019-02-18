e7e0729ce0a599e8919faabe7dd5d4183c80f675
Jheng-lin (Johnny) Jiang <nick2nkite@hotmail.com>
2019-02-18 09:43:06 +0800

    remove "upload" endpoint

--- run.py
+++ run.py
@@ -15,8 +15,7 @@ def allowed_file(filename):
 importer = aurora.SESSION_IMPORTER()
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
+def index():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
